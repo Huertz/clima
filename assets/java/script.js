de6aa0f684f5faefc945f559deea628f33bb5c14
Huertz <104065687+Huertz@users.noreply.github.com>
2022-09-12 23:02:39 -0500

    I had no time to finshi the project

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,37 +1,6 @@
 localStorage.clear();
-
+// Function for findind city
 function findCity() {
     var cityName = titleCase($("#cityName")[0].value.trim());
     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=9352f5f8de34f97f9b8ff0f2b9652df8";
-
-    fetch(apiURL).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-
-                $("#city-name")[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
-
-                $("#city-list").append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + cityName);
-
-                const lat = data.coord.lat;
-                const lon = data.coord.lon;
-
-                var latLonPair = lat.toString() + " " + lon.toString();
-
-                localStorage.setItem(cityName, latLonPair);
-
-                apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=9352f5f8de34f97f9b8ff0f2b9652df8";
-
-                fetch(apiURL).then(function (newResponse) {
-                    if (newResponse.ok) {
-                        newResponse.json().then(function (newData) {
-                            getCurrentWeather(newData);
-                        })
-                    }
-                })
-            })
-        } else {
-            alert("Cannot find city!");
-        }
-    })
 }
-
