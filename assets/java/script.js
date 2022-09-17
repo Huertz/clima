6cb39c2c44bfe1339b84a19079cb8865a5bfa1c8
Huertz <104065687+Huertz@users.noreply.github.com>
2022-09-16 19:38:02 -0500

    added fetch

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,6 +1,28 @@
 localStorage.clear();
 // Function for findind city
 function findCity() {
-    var cityName = titleCase($("#cityName")[0].value.trim());
+    var cityName = $("#cityName")[0].value.trim().toLowerCase();
     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=9352f5f8de34f97f9b8ff0f2b9652df8";
+    fetch(apiURL)
+  .then((response) => response.json())
+  .then((data) => { 
+    console.log(data.main.temp)
+    futureClima (data.coord.lon, data.coord.lat) 
+});
+
 }
+
+$('#search-button').on('click', findCity);
+
+
+
+function futureClima(lon,lat) {
+    var apiURL =  
+    fetch(apiURL)
+  .then((response) => response.json())
+  .then((data) => { 
+    console.log(data) 
+});
+}
+
+// data.list[0].dt_txt.split(' ')[1]
